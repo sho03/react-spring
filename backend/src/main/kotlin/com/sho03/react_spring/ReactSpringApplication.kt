3ca193d6cd686781b7bbd8b8332c003def7880ef
sho ochiai <ochiai_s@welmo.co.jp>
2025-08-30 10:13:32 +0900

    feat: create backend from Spring Initializr.

---
+++ backend/src/main/kotlin/com/sho03/react_spring/ReactSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.sho03.react_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactSpringApplication>(*args)
+}
